52bda16ed7d9bcc6b01f5b614bb5258666dc5198
andrewletterman10 <andrewletterman10@gmail.com>
2020-07-23 17:06:40 -0400

    coommit

--- src/main/java/com/runicrealms/runicauthenticator/runicauthenticator/KeyHandler.java
+++ src/main/java/com/runicrealms/runicauthenticator/runicauthenticator/KeyHandler.java
@@ -18,4 +18,43 @@ public class KeyHandler {
         String format = "https://www.google.com/chart?chs=200x200&chld=M%%7C0&cht=qr&chl=otpauth://totp/%s@%s%%3Fsecret%%3D%s";
         return String.format(format, user, host, secret);
     }
+    public static byte[] toByte(String key){
+        return key.getBytes();
+    }
+    public static int backup_code(
+            byte[] key,
+            long number)
+            throws NoSuchAlgorithmException,
+            InvalidKeyException {
+        byte[] data = new byte[8];
+        long value = number;
+        for (int i = 8; i-- > 0; value >>>= 8) {
+            data[i] = (byte) value;
+        }
+
+
+        SecretKeySpec signKey = new SecretKeySpec(key, "HmacSHA1");
+        Mac mac = Mac.getInstance("HmacSHA1");
+        mac.init(signKey);
+        byte[] hash = mac.doFinal(data);
+
+
+        int offset = hash[20 - 1] & 0xF;
+
+        // We're using a long because Java hasn't got unsigned int.
+        long truncatedHash = 0;
+        for (int i = 0; i < 4; ++i) {
+            truncatedHash <<= 8;
+            // We are dealing with signed bytes:
+            // we just keep the first byte.
+            truncatedHash |= (hash[offset + i] & 0xFF);
+        }
+
+
+        truncatedHash &= 0x7FFFFFFF;
+        truncatedHash %= 1000000;
+
+
+        return (int) truncatedHash;
+    }
 }
--- src/main/java/com/runicrealms/runicauthenticator/runicauthenticator/RunicAuthenticator.java
+++ src/main/java/com/runicrealms/runicauthenticator/runicauthenticator/RunicAuthenticator.java
@@ -23,61 +23,76 @@ import java.util.*;
 
 public final class RunicAuthenticator extends JavaPlugin implements Listener {
 
-        private HashMap<UUID,GoogleAuthenticatorKey> authlockednew= new HashMap<UUID,GoogleAuthenticatorKey>();
-    private ArrayList<UUID> authlocked= new ArrayList<UUID>();
-        @Override
-        public void onEnable() {
-            this.getServer().getPluginManager().registerEvents(this, this);
+    private HashMap<UUID, GoogleAuthenticatorKey> authlockednew = new HashMap<UUID, GoogleAuthenticatorKey>();
+    private ArrayList<UUID> authlocked = new ArrayList<UUID>();
 
-            this.getConfig().options().copyDefaults(true);
-            this.saveConfig();
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+    }
+
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (!this.getConfig().contains("authcodes." + player.getUniqueId()) && player.hasPermission("RunicAuthenticator.required")) {
+            GoogleAuthenticator gAuth = new GoogleAuthenticator();
+            GoogleAuthenticatorKey key = gAuth.createCredentials();
+
+            player.sendMessage("§7Your §bGoogle Auth Code §7is §a" + key.getKey());
+            player.sendMessage(ChatColor.GREEN + "Since You have so much power we need to enable 2 Factor Authentication");
+            //player.sendMessage(url);
+            //Bukkit.broadcastMessage(url);
+            player.sendMessage("§7You must enter this code in the Google Authenticator App before leaving the server.");
+            String url = KeyHandler.getQRBarcodeURL(player.getName(), "Runic Realms", key.getKey());
+            ComponentBuilder message = new ComponentBuilder(ChatColor.BLUE + "" + ChatColor.BOLD + "Click to Copy Link to QR code");
+            BaseComponent[] msg = message.event(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, url)).create();
+            player.spigot().sendMessage(msg);
+            player.sendMessage("§7Then use the code you get in chat to finish this process and log in.");
+            authlocked.add(player.getUniqueId());
+            authlockednew.put(player.getUniqueId(), key);
+        } else if (player.hasPermission("RunicAuthenticator.required")) {
+            authlocked.add(player.getUniqueId());
+            player.sendMessage("§cPlease open the Google Authenticator App and provide the six digit code in chat");
         }
+    }
 
-
-        @EventHandler
-        public void onJoin(PlayerJoinEvent event) {
-            Player player = event.getPlayer();
-
-            if (!this.getConfig().contains("authcodes." + player.getUniqueId())&&player.hasPermission("RunicAuthenticator.required")) {
-                GoogleAuthenticator gAuth = new GoogleAuthenticator();
-                GoogleAuthenticatorKey key = gAuth.createCredentials();
-
-                player.sendMessage("§7Your §bGoogle Auth Code §7is §a" + key.getKey());
-                player.sendMessage(ChatColor.GREEN+"Since You have so much power we need to enable 2 Factor Authentication");
-                //player.sendMessage(url);
-                //Bukkit.broadcastMessage(url);
-                player.sendMessage("§7You must enter this code in the Google Authenticator App before leaving the server.");
-                String url =KeyHandler.getQRBarcodeURL(player.getName(),"Runic Realms",key.getKey());
-                ComponentBuilder message = new ComponentBuilder(ChatColor.BLUE+""+ChatColor.BOLD+"Click to Copy Link to QR code");
-                BaseComponent[] msg = message.event(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, url)).create();
-                player.spigot().sendMessage(msg);
-                player.sendMessage("§7Then use the code you get in chat to finish this process and log in");
-                authlocked.add(player.getUniqueId());
-                authlockednew.put(player.getUniqueId(),key);
-            } else {
-                authlocked.add(player.getUniqueId());
-                player.sendMessage("§cPlease open the Google Authenticator App and provide the six digit code in chat");
-            }
-        }
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
         Player player = event.getPlayer();
     }
-        private boolean playerInputCode(Player player, int code) {
-            String secretkey = this.getConfig().getString("authcodes." + player.getUniqueId());
 
-            GoogleAuthenticator gAuth = new GoogleAuthenticator();
-            boolean codeisvalid = gAuth.authorize(secretkey, code);
+    private boolean playerInputCode(Player player, int code) {
+        String secretkey = this.getConfig().getString("authcodes." + player.getUniqueId());
+
+        GoogleAuthenticator gAuth = new GoogleAuthenticator();
+        boolean codeisvalid = gAuth.authorize(secretkey, code);
 
 
-            if (codeisvalid) {
-                authlocked.remove(player.getUniqueId());
-                return codeisvalid;
-            }
-
+        if (codeisvalid) {
+            authlocked.remove(player.getUniqueId());
             return codeisvalid;
+        }else{
+            try {
+                //for(int i = 0;i<10;i++)
+                if(code==KeyHandler.backup_code(KeyHandler.toByte(secretkey),1)){
+                    return true;
+                }
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (InvalidKeyException e) {
+                e.printStackTrace();
+            }
         }
-    private boolean playerInputCodeNew(Player player, int code,String secretkey) {
+
+        return codeisvalid;
+    }
+
+    private boolean playerInputCodeNew(Player player, int code, String secretkey) {
         GoogleAuthenticator gAuth = new GoogleAuthenticator();
         boolean codeisvalid = gAuth.authorize(secretkey, code);
 
@@ -90,71 +105,72 @@ public final class RunicAuthenticator extends JavaPlugin implements Listener {
         return codeisvalid;
     }
 
-        @EventHandler
-        public void chat(AsyncPlayerChatEvent event) {
-            Player player = event.getPlayer();
-            String message = event.getMessage();
-            if(authlockednew.containsKey(player.getUniqueId())){
-                try {
-                    Integer code = Integer.parseInt(message);
-                    if (playerInputCodeNew(player, code,authlockednew.get(player.getUniqueId()).getKey())) {
-                        authlocked.remove(player.getUniqueId());
-                        player.sendMessage(ChatColor.DARK_GREEN +"*Access Granted* §bWelcome to the server!");
-                        if(!this.getConfig().contains("authcodes." + player.getUniqueId())){
-                            player.sendMessage(ChatColor.GREEN+"Sorry for the hassle!");
-                            this.getConfig().set("authcodes." + player.getUniqueId(), authlockednew.get(player.getUniqueId()).getKey());
-                            this.saveConfig();
-                            authlockednew.remove(player.getUniqueId());
-                            event.setCancelled(true);
-                            return;
-                        }
-                    } else {
-                        player.sendMessage(ChatColor.RED+"Incorrect or expired code ** A code will only contain numbers **");
+    @EventHandler
+    public void chat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+        if (authlockednew.containsKey(player.getUniqueId())) {
+            try {
+                Integer code = Integer.parseInt(message);
+                if (playerInputCodeNew(player, code, authlockednew.get(player.getUniqueId()).getKey())) {
+                    authlocked.remove(player.getUniqueId());
+                    player.sendMessage(ChatColor.DARK_GREEN + "*Access Granted* §bWelcome to the server!");
+                    if (!this.getConfig().contains("authcodes." + player.getUniqueId())) {
+                        player.sendMessage(ChatColor.GREEN + "Sorry for the hassle!");
+                        player.sendMessage(ChatColor.BLUE+"Your backup code is "+KeyHandler.backup_code(KeyHandler.toByte(authlockednew.get(player.getUniqueId()).getKey()),1));
+                        this.getConfig().set("authcodes." + player.getUniqueId(), authlockednew.get(player.getUniqueId()).getKey());
+                        this.saveConfig();
+                        authlockednew.remove(player.getUniqueId());
+                        event.setCancelled(true);
+                        return;
                     }
-            }catch (Exception e) {
-                    player.sendMessage(ChatColor.RED+"Incorrect or expired code ** A code will only contain numbers **");
+                } else {
+                    player.sendMessage(ChatColor.RED + "Incorrect or expired code ** A code will only contain numbers **");
                 }
-                event.setCancelled(true);
-                return;
+            } catch (Exception e) {
+                player.sendMessage(ChatColor.RED + "Incorrect or expired code ** A code will only contain numbers **");
             }
-            if (authlocked.contains(player.getUniqueId())) {
-                try {
-                    Integer code = Integer.parseInt(message);
-                    if (playerInputCode(player, code)) {
-                        authlocked.remove(player.getUniqueId());
-                        player.sendMessage(ChatColor.DARK_GREEN +"*Access Granted* §bWelcome to the server!");
-                    } else {
-                        player.sendMessage(ChatColor.RED+"Incorrect or expired code ** A code will only contain numbers **");
-                    }
-                } catch (Exception e) {
-                    player.sendMessage(ChatColor.RED+"Incorrect or expired code ** A code will only contain numbers **");
+            event.setCancelled(true);
+            return;
+        }
+        if (authlocked.contains(player.getUniqueId())) {
+            try {
+                Integer code = Integer.parseInt(message);
+                if (playerInputCode(player, code)) {
+                    authlocked.remove(player.getUniqueId());
+                    player.sendMessage(ChatColor.DARK_GREEN + "*Access Granted* §bWelcome to the server!");
+                } else {
+                    player.sendMessage(ChatColor.RED + "Incorrect or expired code ** A code will only contain numbers **");
                 }
-                event.setCancelled(true);
+            } catch (Exception e) {
+                player.sendMessage(ChatColor.RED + "Incorrect or expired code ** A code will only contain numbers **");
             }
+            event.setCancelled(true);
         }
+    }
 
-        @EventHandler
-        public void command(PlayerCommandPreprocessEvent event) {
-            Player player = event.getPlayer();
-            if (authlocked.contains(player.getUniqueId())) {
-                event.setCancelled(true);
-                player.sendMessage(ChatColor.RED+"Ah Ah Ah, you didnt say the magic word!");
-            }
+    @EventHandler
+    public void command(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        if (authlocked.contains(player.getUniqueId())) {
+            event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "Ah Ah Ah, you didnt say the magic word!");
         }
+    }
 
-        @EventHandler
-        public void blockbreak(BlockBreakEvent event) {
-            Player player = event.getPlayer();
-            if (authlocked.contains(player.getUniqueId())) {
-                event.setCancelled(true);
-            }
+    @EventHandler
+    public void blockbreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        if (authlocked.contains(player.getUniqueId())) {
+            event.setCancelled(true);
         }
+    }
 
-        @EventHandler
-        public void blockplace(BlockPlaceEvent event) {
-            Player player = event.getPlayer();
-            if (authlocked.contains(player.getUniqueId())) {
-                event.setCancelled(true);
-            }
+    @EventHandler
+    public void blockplace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        if (authlocked.contains(player.getUniqueId())) {
+            event.setCancelled(true);
         }
+    }
 }
